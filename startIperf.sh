b0f3caa438e8d6f7affc95b5a77215be6118e9cc
Eleanor Davies <ellie@mavieson.co.uk>
2019-09-12 18:16:13 +0100

    demo

--- startIperf.sh
+++ startIperf.sh
@@ -1,3 +1,3 @@
 echo $1
-iperf3 -c "$1" -t 10
+iperf3 -c "$1" -t "$2" -u -b "$3"
 # tmux new-session -d -s iperf 'python iperf.py $1'
--- trafficGen.py
+++ trafficGen.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
-import sys, getopt, subprocess, atexit, threading, time
+import sys, getopt, subprocess, atexit, threading, time, random
 from config import HOSTS, IPERF_HOSTS, IPERF_SERVER, DURATION
 
+HEADER      = '\033[95m'
+OKBLUE      = '\033[94m'
+OKGREEN     = '\033[92m'
+WARNING     = '\033[93m'
+FAIL        = '\033[91m'
+ENDC        = '\033[0m'
+BOLD        = '\033[1m'
+UNDERLINE   = '\033[4m'
+GIGABIT     = "1073741824"
+
 def goodbye():
-    print("Stopping iperf sessions...\n")
+    print("Stopping iperf sessions...")
     for i in range(len(HOSTS)):
         print("Stopping iperf server on " + HOSTS[i])
         run_command(HOSTS[i], "stopServer.sh")   
@@ -12,36 +22,82 @@ def goodbye():
 def run_command(host, command):
     subprocess.call("ssh sdn@" + host + " -o StrictHostKeyChecking=no -t 'source ~/.bash_profile && cd traffic && bash " + command + "'", shell=True, stdout=subprocess.PIPE)
 
-def main():
-    atexit.register(goodbye)
+def start_servers(hosts):
     # Start iperf3 server deamons on all hosts
-    for i in range(len(IPERF_SERVER)):
-        print("Starting iperf server on " + IPERF_SERVER[i])
-        run_command(IPERF_SERVER[i], "startServer.sh")
+    for i in range(len(hosts)):
+        print("Starting iperf server on " + hosts[i])
+        run_command(hosts[i], "startServer.sh")
 
-    print("Starting default iperf sessions [10.0.0.1 -> 10.0.0.3] and [10.0.0.2 -> 10.0.0.4] for 600 seconds (10 mins)")
-    print("More options coming soon...\n")
+def default_iperf():
+    print(OKGREEN + "Starting default iperf sessions on 2 threads [10.0.0.1 -> 10.0.0.3] and [10.0.0.2 -> 10.0.0.4] for " + str(DURATION) + " seconds" + ENDC)
+    iperf_exec(GIGABIT)
 
+def iperf_exec(speed):
     threads = []
 
     for i in range(len(IPERF_HOSTS)):
         try:
-            thread = threading.Thread(target=run_command, args=(IPERF_HOSTS[i], "startIperf.sh " + IPERF_SERVER[i]))
-            thread.daemon = True                            # Daemonize thread
+            print(OKBLUE + "Speed: " + str(int(speed)/1000000) + "Mbps" + ENDC)
+            thread = threading.Thread(target=run_command, args=(IPERF_HOSTS[i], "startIperf.sh " + IPERF_SERVER[i] + " " + str(DURATION) + " " + speed))
+            thread.daemon = True                          
             thread.start()
-            # thread.start_new_thread(run_command, (IPERF_HOSTS[i], "startIperf.sh " + IPERF_SERVER[i]))
         except:
             print("unable to start thread")
-        # run_command(IPERF_HOSTS[i], "startIperf.sh " + IPERF_SERVER[i])
 
-    time.sleep(DURATION)
+    time.sleep(DURATION + 1)
     for thread in threads:
         thread.join()
 
     subprocess.call("stty sane", shell=True)
 
-    #input("Press Enter to quit...")
+def random_iperf():
+    print("Please enter the number of iterations:")
+    itr = input(">")
+    try:
+        itr = int(itr)
+    except:
+        print(WARNING + "Please enter a number" + ENDC)
+        return
+    print("Performing " + str(itr) + " iterations, " + str(DURATION) + " seconds long each for a total test time of " + str(DURATION * itr) + " seconds")
+    print(OKGREEN + "Starting iperf sessions on 2 threads [10.0.0.1 -> 10.0.0.3] and [10.0.0.2 -> 10.0.0.4]" + ENDC)
 
+    for i in range(itr):
+        iperf_exec(str(random.randrange(int(GIGABIT))))
+
+
+def menu():
+    print("Please choose an option:")
+    print("d: Default options")
+    print("r: Random test")
+    print("m: Manual mode")
+    print("q: quit")
+    res = input(">")
+    if res == "d":
+        default_iperf()
+    elif res == "r":
+        random_iperf()
+    elif res == "m":
+        print("Functionality coming soon...")
+    elif res == "q":
+        return
+    else:
+        print(WARNING + "Not a valid option, please try again." + ENDC)
+        menu()
+    menu()
+
+def main():
+    atexit.register(goodbye)
+    start_servers(HOSTS)
+    print(OKBLUE + "\n   ____  _   _  ____   _____  \n  / __ \| \ | |/ __ \ / ____| \n | |  | |  \| | |  | | (___   \n | |  | | . ` | |  | |\___ \  \n | |__| | |\  | |__| |____) | \n  \____/|_| \_|\____/|_____/  \n" + ENDC)
+    
+    print(BOLD + "There are currently " + str(len(HOSTS)) + " hosts available:" + ENDC)
+    for i in range(len(HOSTS)):
+        print("Host: " + HOSTS[i])
+    print("The duration variable is currently set to " + str(DURATION) + " seconds. Change this in config.py")
+    menu()   
+    
+
+    #input("Press Enter to quit...")
 
 if __name__ == "__main__":
    main()
