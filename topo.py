4376bd8db2dc63433aeff596f42e33ba5bfc8b47
Eleanor Davies <ellie@mavieson.co.uk>
2020-03-20 15:19:44 +0000

    pour will

--- topo.py
+++ topo.py
@@ -28,47 +28,47 @@ from mininet.node import OVSKernelSwitch
 #         self.addLink(h[2], s[1], bw=bw)
 #         self.addLink(h[3], s[1], bw=bw)
 
-def connectToRootNS( network, switch, ip, routes ):
-    """Connect hosts to root namespace via switch. Starts network.
-      network: Mininet() network object
-      switch: switch to connect to root namespace
-      ip: IP address for root namespace node
-      routes: host networks to route to"""
-    # Create a node in root namespace and link to switch 0
-    root = Node( 'root', inNamespace=False )
-    intf = network.addLink( root, switch ).intf1
-    root.setIP( ip, intf=intf )
-    # Start network that now includes link to root namespace
-    network.start()
-    # Add routes from root ns to hosts
-    for route in routes:
-        root.cmd( 'route add -net ' + route + ' dev ' + str( intf ) )
+# def connectToRootNS( network, switch, ip, routes ):
+#     """Connect hosts to root namespace via switch. Starts network.
+#       network: Mininet() network object
+#       switch: switch to connect to root namespace
+#       ip: IP address for root namespace node
+#       routes: host networks to route to"""
+#     # Create a node in root namespace and link to switch 0
+#     root = Node( 'root', inNamespace=False )
+#     intf = network.addLink( root, switch ).intf1
+#     root.setIP( ip, intf=intf )
+#     # Start network that now includes link to root namespace
+#     network.start()
+#     # Add routes from root ns to hosts
+#     for route in routes:
+#         root.cmd( 'route add -net ' + route + ' dev ' + str( intf ) )
 
-def sshd( network, cmd='/usr/sbin/sshd', opts='-D',
-          ip='10.123.123.1/32', routes=None, switch=None ):
-    """Start a network, connect it to root ns, and run sshd on all hosts.
-       ip: root-eth0 IP address in root namespace (10.123.123.1/32)
-       routes: Mininet host networks to route to (10.0/24)
-       switch: Mininet switch to connect to root namespace (s1)"""
-    if not switch:
-        switch = network[ 's1' ]  # switch to use
-    if not routes:
-        routes = [ '10.0.0.0/24' ]
-    connectToRootNS( network, switch, ip, routes )
-    for host in network.hosts:
-        host.cmd( cmd + ' ' + opts + '&' )
-    info( "*** Waiting for ssh daemons to start\n" )
-    for server in network.hosts:
-        waitListening( server=server, port=6767, timeout=5 )
+# def sshd( network, cmd='/usr/sbin/sshd', opts='-D',
+#           ip='10.123.123.1/32', routes=None, switch=None ):
+#     """Start a network, connect it to root ns, and run sshd on all hosts.
+#        ip: root-eth0 IP address in root namespace (10.123.123.1/32)
+#        routes: Mininet host networks to route to (10.0/24)
+#        switch: Mininet switch to connect to root namespace (s1)"""
+#     if not switch:
+#         switch = network[ 's1' ]  # switch to use
+#     if not routes:
+#         routes = [ '10.0.0.0/24' ]
+#     connectToRootNS( network, switch, ip, routes )
+#     for host in network.hosts:
+#         host.cmd( cmd + ' ' + opts + '&' )
+#     info( "*** Waiting for ssh daemons to start\n" )
+#     for server in network.hosts:
+#         waitListening( server=server, port=6767, timeout=5 )
 
-    info( "\n*** Hosts are running sshd at the following addresses:\n" )
-    for host in network.hosts:
-        info( host.name, host.IP(), '\n' )
-    info( "\n*** Type 'exit' or control-D to shut down network\n" )
-    CLI( network )
-    for host in network.hosts:
-        host.cmd( 'kill %' + cmd )
-    network.stop()
+#     info( "\n*** Hosts are running sshd at the following addresses:\n" )
+#     for host in network.hosts:
+#         info( host.name, host.IP(), '\n' )
+#     info( "\n*** Type 'exit' or control-D to shut down network\n" )
+#     CLI( network )
+#     for host in network.hosts:
+#         host.cmd( 'kill %' + cmd )
+#     network.stop()
 
 if __name__ == '__main__':   
     net = Mininet(controller=RemoteController, link=TCLink)
